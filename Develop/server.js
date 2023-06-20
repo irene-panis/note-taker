50a12b9c9b485de34b4af904a47153e9b4fffd1b
irene <panisirene@gmail.com>
2023-06-20 16:36:27 -0700

    add fallback path and port listen

--- Develop/server.js
+++ Develop/server.js
@@ -23,3 +23,12 @@ app.get('/api/notes', (req, res) => {
     res.json(JSON.parse(response));
   });
 });
+
+// fallback path for paths that dont exist
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Express server listening on port ${PORT}!`);
+});
